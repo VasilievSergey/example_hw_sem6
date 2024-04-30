550645223c782a51c82572bceff871cfc02b1f29
Васильев Сергей <9134829617@mail.ru>
2024-04-30 20:11:39 +0700

    Initial commit

---
+++ src/main/java/ru/gb/example_hw_sem6/ExampleHwSem6Application.java
@@ -0,0 +1,15 @@
+package ru.gb.example_hw_sem6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// localhost:8080/swagger-ui/index.html
+
+@SpringBootApplication
+public class ExampleHwSem6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleHwSem6Application.class, args);
+	}
+
+}
